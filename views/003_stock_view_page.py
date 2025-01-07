ce7071668cf402313aef90acfb57c5c7850edd7d
Mihály Orsós <ormraat.pte@gmail.com>
2025-01-07 18:19:01 +0100

    Update 003_stock_view_page.py

--- views/003_stock_view_page.py
+++ views/003_stock_view_page.py
@@ -11,7 +11,7 @@ def get_tw():
 tw = get_tw()
 
 def update_index():
-    st.session_state.current_index = int(tw.stock[tw.stock['display_name'] == st.session_state.swstock].index[0])
+    st.session_state.sw_current_index = int(tw.stock[tw.stock['display_name'] == st.session_state.swstock].index[0])
 
 # Initialize session state for sw_current_index
 if "sw_current_index" not in st.session_state:
@@ -24,8 +24,7 @@ def stock_dashboard():
     """Stock Dashboard for displaying stock details and visualizations."""
 
     # First row: Dropdown menu for stock selection and additional info
-    col1, col2, col3 = st.columns([2, 1.4, 2])
-
+    col1, col2, col3, col4 = st.columns([2, 1.4, 1.3, 1.3], vertical_alignment="center")
     with col1:
         with st.container(border=True):
             st.markdown("##### Select a Stock")
@@ -38,7 +37,8 @@ def stock_dashboard():
             )
             
             label_of_next_button = f'Next: {tw.stock["display_name"][(st.session_state.sw_current_index + 1) % len(tw.stock)]}'
-
+    with col2:
+        with st.container(border=True):
             # next button
             if st.button(label_of_next_button):
                 st.session_state.sw_current_index = (st.session_state.sw_current_index + 1) % len(tw.stock)
@@ -50,7 +50,7 @@ def stock_dashboard():
     company_data = tw.stock.loc[st.session_state.sw_current_index]
     
 
-    with col2:
+    with col3:
         with st.container(border=True):
             st.markdown("##### More Info")
             tradingview_url = f"https://www.tradingview.com/chart/?symbol={user_ticker}"
@@ -62,24 +62,24 @@ def stock_dashboard():
             )
 
     # Second row: Displaying stock metrics
-    st.markdown("### Stock Metrics")
-    metrics = [
-        {"label": "Price", "value": round(company_data["close"], 2)},
-        {"label": "Price-to-Earnings (P/E)", "value": round(company_data["price_earnings_ttm"], 2)},
-        {"label": "52-Week High", "value": f"${round(company_data['price_52_week_high'], 2)}"},
-        {"label": "52-Week Low", "value": f"${round(company_data['price_52_week_low'], 2)}"},
-        {"label": "RSI", "value": int(company_data["RSI7"])},
-    ]
+    with st.expander('### Stock Metrics', expanded=True):
+        metrics = [
+            {"label": "Price", "value": round(company_data["close"], 2)},
+            {"label": "Price-to-Earnings (P/E)", "value": round(company_data["price_earnings_ttm"], 2)},
+            {"label": "52-Week High", "value": f"${round(company_data['price_52_week_high'], 2)}"},
+            {"label": "52-Week Low", "value": f"${round(company_data['price_52_week_low'], 2)}"},
+            {"label": "RSI", "value": int(company_data["RSI7"])},
+        ]
 
-    col1, col2, col3, col4, col5 = st.columns(5)
+        col1, col2, col3, col4, col5 = st.columns(5)
 
-    # Displaying metrics in containers
-    for col, metric in zip([col1, col2, col3, col4, col5], metrics):
-        with col:
-            with st.container(border=True):
-                st.metric(label=metric["label"], value=metric["value"])
+        # Displaying metrics in containers
+        for col, metric in zip([col1, col2, col3, col4, col5], metrics):
+            with col:
+                with st.container(border=True):
+                    st.metric(label=metric["label"], value=metric["value"])
 
-    st.divider()
+        st.divider()
 
     # Third row: Stock price chart
     st.markdown("### Stock Price Over the Last Year")
